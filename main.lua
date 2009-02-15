b7e4abc8c0b59fbcfd2b623cb3b9285f1e703617
Pontus Granström <pnutus@gmail.com>
2009-02-15 18:19:46 +0100

    Made it a game, complete with a game over screen and scoring.

--- main.lua
+++ main.lua
@@ -12,8 +12,8 @@ function load()
 	chaped = love.physics.newCircleShape(chased, 15)
 	chased:setMassFromShapes()
 	
-	chaserX = 300
-	chaserY = 400
+	chaserX = 0
+	chaserY = 0
 	chasedX = 400
 	chasedY = 300
 	
@@ -27,47 +27,76 @@ function load()
 	chaserSpeed = 100
 	chasedSpeed = 200
 	
+	timePassed = 0,1
+	gameOver = false
+	endTime = 0
+	
 	chaserImg = love.graphics.newImage("chaser.png")
 	chasedImg = love.graphics.newImage("chased.png")
+	
+	loseFont = love.graphics.newFont(love.default_font, 40)
+	font = love.graphics.newFont(love.default_font, 14)
+	miniFont = love.graphics.newFont(love.default_font, 7)
+	love.graphics.setFont(font)
 end
 
 function update(dt)
-	world:update(dt)
+	timePassed = timePassed + dt
 	
-	if love.keyboard.isDown(love.key_left) then
-		chasedX = chasedX - chasedSpeed * dt
-	end
-	if love.keyboard.isDown(love.key_right) then
-		chasedX = chasedX + chasedSpeed * dt
-	end
-	if love.keyboard.isDown(love.key_up) then
-		chasedY = chasedY - chasedSpeed * dt
-	end
-	if love.keyboard.isDown(love.key_down) then
-		chasedY = chasedY + chasedSpeed * dt
-	end
 	
-	if (chasedY - chasedX) ~= (chasedSpeed * dt) then
+		mouseX, mouseY = love.mouse.getPosition()
 		
+		deltaX = chasedX - chaserX
+		deltaY = chasedY - chaserY
+		
+		hypotenuse = (deltaY^2 + deltaX^2)^0.5
+    	
+		chaserX = chaserX + deltaX * chaserSpeed * dt / hypotenuse
+		chaserY = chaserY + deltaY * chaserSpeed * dt / hypotenuse
+		
+		chaser:applyForce(chaserX, chaserY)
+		chased:applyForce(chasedX, chasedY)
+		
+	if hypotenuse < 10 then
+		if gameOver == false then
+			endTime = timePassed
+		end
+		gameOver = true
+	else
+		world:update(dt)
+		
+		if love.keyboard.isDown(love.key_left) then
+			chasedX = chasedX - chasedSpeed * dt
+		end
+		if love.keyboard.isDown(love.key_right) then
+			chasedX = chasedX + chasedSpeed * dt
+		end
+		if love.keyboard.isDown(love.key_up) then
+			chasedY = chasedY - chasedSpeed * dt
+		end
+		if love.keyboard.isDown(love.key_down) then
+			chasedY = chasedY + chasedSpeed * dt
+		end
+		
+		if (chasedY - chasedX) ~= (chasedSpeed * dt) then
+			
+		end
 	end
-	
-	mouseX, mouseY = love.mouse.getPosition()
-	
-	deltaX = chasedX - chaserX
-	deltaY = chasedY - chaserY
-	
-	hypotenuse = (deltaY^2 + deltaX^2)^0.5
-	
-	chaserX = chaserX + deltaX * chaserSpeed * dt / hypotenuse
-	chaserY = chaserY + deltaY * chaserSpeed * dt / hypotenuse
-	
-	chaser:applyForce(chaserX, chaserY)
-	chased:applyForce(chasedX, chasedY)
 end
 
 function draw()
-	love.graphics.draw(chaserImg, chaserX, chaserY)
-	love.graphics.draw(chasedImg, chasedX, chasedY)
---	love.graphics.draw(chaserImg, chaser:getX(), chaser:getY(), chaser:getAngle())
---	love.graphics.draw(chasedImg, chased:getX(), chased:getY(), chased:getAngle())
+	if gameOver then
+		love.graphics.setFont(loseFont)
+		love.graphics.draw("YOU LOSE!!!", 150, 240)
+		love.graphics.setFont(font)
+		love.graphics.drawf("You survived for "..math.floor(endTime).." seconds. Not good enough for a place in the high-score list*.", 150, 275, 300)
+		love.graphics.setFont(miniFont)
+		love.graphics.draw("*Partly because there isn't any.", 510, 475)
+	else
+		love.graphics.draw("You have survived for "..math.floor(timePassed).." seconds. So far...", 10, 20)
+		love.graphics.draw(chaserImg, chaserX, chaserY)
+		love.graphics.draw(chasedImg, chasedX, chasedY)
+--		love.graphics.draw(chaserImg, chaser:getX(), chaser:getY(), chaser:getAngle())
+--		love.graphics.draw(chasedImg, chased:getX(), chased:getY(), chased:getAngle())
+	end
 end
